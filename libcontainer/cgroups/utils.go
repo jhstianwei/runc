22fbd8683f45034fee234e69e7924e949d7f6a54
tw108174 <tw108174@alibaba-inc.com>
2019-07-16 11:33:19 +0800

    save code

--- libcontainer/cgroups/utils.go
+++ libcontainer/cgroups/utils.go
@@ -348,6 +348,10 @@ func EnterPid(cgroupPaths map[string]string, pid int) error {
 //
 func RemovePaths(paths map[string]string) (err error) {
 	delay := 10 * time.Millisecond
+	for s, p := range paths {
+	   fmt.Println("s:" + s + ", p:" + p)
+	   delete(paths, s)
+	}
 	delete(paths, "memory")
 	fmt.Println("delete paths")
 	for i := 0; i < 5; i++ {
